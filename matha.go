a633bfa5525cd24c00cf343ac48954e001020b85
second_dev <second_dev@mail.ru>
2023-04-28 17:48:23 +0400

    PrintNumbers

--- matha.go
+++ matha.go
@@ -1,9 +1,15 @@
 package gopackage_calc
 
+import "fmt"
+
 func Add(a, b int) int {
 	return a + b
 }
 
 func Multi(a, b int) int {
 	return a + b
+}
+
+func PrintNumbers(a, b int) {
+	fmt.Println(a,b)
 }
